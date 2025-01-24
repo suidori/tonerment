61f1434275fed38c91676947e0fad0748d2a7d6c
suidori <leo7750@naver.com>
2025-01-24 17:23:07 +0900

    0124 tonamel

---
+++ tonerment/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header">
+  <div id="register">회원가입</div>
+  <div id="login">로그인</div>
+  </div>
+  <div id="main">
+  <div id="left">
+  왼쪽 디자인
+  </div>
+  <div id="right">
+  오른쪽 메인
+  </div>
+  </div>
+</template>
+
+<script setup>
+import './css/main.css'
+</script>
+
+<style lang="scss" scoped></style>
